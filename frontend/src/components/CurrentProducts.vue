71229b8ec08150342e7c4921696864d99314faf8
Stanislav Frantsuzov <s.frantsuzov@wespell.com>
2025-05-02 14:35:12 +0200

    added new info

--- frontend/src/components/CurrentProducts.vue
+++ frontend/src/components/CurrentProducts.vue
@@ -4,6 +4,7 @@
       <v-list-item v-for="(product, index) in products" :key="index">
         <v-list-item-content>
           <v-list-item-title>{{ product.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ product.code }} &bull; {{ product.weight }} &bull; {{ product.serial }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -12,9 +13,9 @@
 
 <script setup>
 const products = [
-  { name: 'Черный чай' },
-  { name: 'Зеленый чай' },
-  { name: 'Травяной сбор' }
+  {name: 'Черный чай', code: 'BT-1449', weight: '500г', serial: 'A14491'},
+  {name: 'Зеленый чай', code: 'GT-7845', weight: '250г', serial: 'Р78452'},
+  {name: 'Травяной сбор', code: 'HT-158', weight: '100г', serial: 'Р1583'}
 ]
 </script>
 
